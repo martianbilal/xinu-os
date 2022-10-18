a6a21259a07af29585a8faffce3100b5ec4f47a0
Bilal Naeem <2016bilal1990@gmail.com>
2022-10-18 00:53:46 -0400

    Add inspectmaxprio

---
+++ system/inspectmaxprio.c
@@ -0,0 +1,34 @@
+/* inspectmaxprio.c - inspectmaxprio */
+
+#include <xinu.h>
+
+
+/*------------------------------------------------------------------------
+ *  inspectmaxprio  -  return the max priority of the ready list
+ *------------------------------------------------------------------------
+ */
+pri16 inspectmaxprio(
+    void
+)
+{
+	intmask 	mask;    	/* Interrupt mask		*/
+	// // struct	procent *prptr;		/* Ptr to process's table entry	*/
+    struct mfeedbqx *dynptr;  /* Ptr to dynamic queue entry */
+    int i;  /* for loop iterator */
+
+    mask = disable();
+    
+    for(i = 9; i >= 0; i--) {
+        dynptr = &dynqueue[i];
+        if(dynptr->count){
+            // success
+            restore(mask);
+            return i;
+        }
+    }
+
+    // failure
+    restore(mask);
+    return SYSERR;
+}
+
