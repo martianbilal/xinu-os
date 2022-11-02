58a574b486d196eaec89499378b629e2720a4359
Bilal Naeem <2016bilal1990@gmail.com>
2022-11-02 11:48:20 -0400

    Add executedetour2

---
+++ system/executedetour2.c
@@ -0,0 +1,44 @@
+/* executedetour2.c - executedetour2 */
+
+#include <xinu.h>
+
+/*------------------------------------------------------------------------
+ *  executedetour2  - execute detour2
+ *------------------------------------------------------------------------
+ */
+void	executedetour2(void)
+{
+	intmask	mask;			/* Saved interrupt mask		*/
+	struct procent *prptr;		/* Ptr to process' table entry	*/
+
+
+	mask = disable();
+
+	prptr = &proctab[currpid];
+	kprintf("process ready to execute detour2 %s\n", prptr->prname);
+
+	
+	if(prptr->prmakedetour == 1)
+	{
+		kprintf("process ready to execute detour2 %s\n", prptr->prname);
+	}
+
+	
+	if(prptr->prmakedetour == 1) {
+		kprintf("executedetour2: %s\n", prptr->prname);
+		prptr->prmakedetour = 0;
+		prptr->prnumalarms = prptr->prnumalarms - 1;
+
+		// [BILAL] call the callback function
+		(*prptr->prcbftn)();
+		restore(mask);
+		return;
+	} else {
+		restore(mask);
+		return;
+	}
+
+	
+	restore(mask);
+	return;
+}
